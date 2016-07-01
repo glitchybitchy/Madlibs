e4d7672aec816192cf8498da95f130b26066018e
glitchybitchy <woznarowycz@gmail.com>
2016-07-01 23:03:56 +0100

    added arrays

---
+++ madlibs.js
@@ -0,0 +1,7 @@
+var myStory = 'Once upon a time there was a [adjective] [noun]. It was really [adjective]. It liked to [verb] all day. One day, it went to [place] to meet [person]. To get there, it rode in a [vehicle], but on the way there, the [vehicle] crashed.  It had to walk the rest of the way.'
+
+var someAdjectives = ['beautiful', 'nasty', 'eloquent', 'purple', 'rumbunctious', 'shady'];
+var someNouns = ['goat', 'lamp', 'candy', 'shovel', 'monster truck', 'computer'];
+var somePeople = ['Mabel', 'Charlie', 'Gruncle Stan', 'Frodo', 'Princess Bubblegum', 'Bill Cypher' ];
+var somePlaces = ['Candy Kingdom', 'Gravity Falls', 'Philadelphia', 'Mordor', 'Rivendell', 'Essex' ];
+var someVerbs = ['ate', 'ran', 'bounced', 'laughed', 'slapped', 'broke'];
